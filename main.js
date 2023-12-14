9471432c3392f6a20ebe080c30216cb6dae69753
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-12-14 19:05:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+x= 0;
+y = 0;
+drawCircle = "";
+drawRect = "";
+
+function start()
+{
+    document.getElementById("status").innerHTML = "O sistema está ouvindo. Por favor, fale."; 
+    recognition.start();
+} 
+
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(900,600)
+}
+function draw(){
+
+}
